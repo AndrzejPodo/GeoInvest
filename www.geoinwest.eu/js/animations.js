6a08aaa0ee38ba55feaac9e65844bb1b6ee2ed37
Lidia Wiśniewska <lidkaa.wu@gmail.com>
2018-06-22 12:39:57 +0200

    Commit message lol jestem Lidzia zla mi sie

---
+++ www.geoinwest.eu/js/animations.js
@@ -0,0 +1,91 @@
+﻿$(window).on("load", function () {
+    //--------------------------------------------------------------------scroll
+    $("#arrow, #click1, #a1").click(function () {
+        $("body, html").animate({
+            scrollTop: ($("#one").offset().top - 25),
+        }, 1000);
+    });
+    $("#click3, #a2").click(function () {
+        $("body, html").animate({
+            scrollTop: ($("#two").offset().top - 20),
+        }, 1000);
+    });
+    $("#o_nas a, #click2, #click5").click(function () {
+        $("body, html").animate({
+            scrollTop: ($("#footer").offset().top - 450),
+        }, 1000);
+    });
+    $("#podsumowanie h4").click(function () {
+        $("body, html").animate({
+            scrollTop: ($("#header").offset().top),
+        }, 1000);
+    });
+    $("#click4, #a3").click(function () {
+        $("body, html").animate({
+            scrollTop: ($("#nasze_domy").offset().top),
+        }, 1000);
+    });
+    //--------------------------------------------------------------------animations
+    $(window).scroll(function () {
+        var i = 0;
+        var j = 0;
+        var k = 0;
+
+        if ($(this).scrollTop() > innerHeight / 4) {
+            if (innerWidth < 800) {
+                if (i < 10) {
+                    $("#o_nas_txt").animate({
+                        marginLeft: "+5",
+                        opacity: 1,
+                    }, 2000);
+                    i++;
+                }
+            }
+            else {
+                if (i < 10) {
+                    $("#o_nas_txt").animate({
+                        marginLeft: "+100",
+                        opacity: 1,
+                    }, 2000);
+                    i++;
+                }
+            }
+            
+        }
+        if ($(this).scrollTop() > (innerHeight / 4) + innerHeight) {
+            if (j < 3) {
+                $("#galeria h2").animate({
+                    marginLeft: "700",
+                    opacity: 1,
+                }, 2000);
+                j++;
+            }
+
+        }
+        if (innerWidth < 800) {
+            if ($(this).scrollTop() > (innerHeight / 4) + (innerHeight)) {
+                if (k < 10) {
+                    $("#nasze_domy_txt").animate({
+                        marginLeft: "+5",
+                        opacity: 1,
+                    }, 2000);
+                    k++;
+                }
+            }
+        }
+        else{
+            if ($(this).scrollTop() > (innerHeight / 4) + (2 * innerHeight)) {
+                if (k < 10) {
+                    $("#nasze_domy_txt").animate({
+                        marginLeft: "+100",
+                        opacity: 1,
+                    }, 2000);
+                    k++;
+
+                }
+            }
+        }
+       
+      
+    });
+});
---
+++ www.geoinwest.eu/js/map.js
@@ -0,0 +1,32 @@
+﻿$(window).on("load", function () {
+    $("#mapa").gmap3({
+        map: {
+            options: {
+                center: [49.675987588153006, 19.623264321126044],
+                zoom: 11,
+                disableDefaultUI: true
+
+            },
+        },
+        marker: {        
+        options: {
+                draggable: true
+        },
+        latLng: [49.675987588153006, 19.623264321126044]
+
+    }
+    })
+
+    $("#mapa").gmap3({
+        map: {
+            events: {
+                click: function (map, event) {
+                    console.log(event.latLng.lat());
+                    console.log(event.latLng.lng());
+                    console.log(map.getZoom());
+
+                }
+            }
+        }
+    });
+})
