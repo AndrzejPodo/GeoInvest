08b113ade7040f9ab31a0e98c246b9e76631337e
AndrzejPodo <andrzejpodobinski2@gmail.com>
2019-02-14 14:31:33 +0100

    sekcja o nas z animacja

--- www.geoinwest.eu/animations.js
+++ www.geoinwest.eu/animations.js
@@ -4,4 +4,18 @@ $(document).ready(function(){
     }, function(){
         $(this).find('p').animate({fontSize: "0px"})
     })
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() > innerHeight/4 && innerWidth > 960){
+            $('#onas-h4').animate({marginLeft:"50px", opacity:1},1500);
+            $('#onas-p').animate({marginLeft:"50px",opacity:1},1500);
+        }else{
+            $('#onas-h4').animate({marginLeft:"30px", opacity:1},1500);
+            $('#onas-p').animate({marginLeft:"30px",opacity:1},1500);
+        }
+    });
+
+    setInterval(function(){
+        console.log($('#onas-p').css('margin-left'));
+    },1000);
 })
