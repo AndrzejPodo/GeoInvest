eef6a7c7d73affdf1c90e26c773604000cd03bfd
AndrzejPodo <andrzejpodobinski2@gmail.com>
2019-02-14 19:04:49 +0100

    pierwsza wersja galerii

--- www.geoinwest.eu/animations.js
+++ www.geoinwest.eu/animations.js
@@ -12,6 +12,13 @@ $(document).ready(function(){
         },1000);
     });
 
+    $('#galleryButton').click(function(){
+        console.log("INFO");
+        $('html, body').animate({
+            scrollTop: ($('#galleryLine').offset().top)
+        },1000);
+    });
+
     $(window).scroll(function(){
         if($(window).scrollTop() > innerHeight/4 && innerWidth > 960){
             $('#onas-h4').animate({marginLeft:"50px", opacity:1},1500);
@@ -22,7 +29,12 @@ $(document).ready(function(){
         }
     });
 
-    setInterval(function(){
-        console.log($('#onas-p').css('margin-left'));
-    },1000);
+    $('.galleryImg').click(function(){
+        $('#myModal').css({'display':'block'});
+        $('#img01').attr('src',this.src);
+    });
+
+    $('.close').click(function(){
+        $('#myModal').css({'display':'none'});
+    });
 })
